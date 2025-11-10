0539b93597d8c78de5fb060e4bffcaf25b14d0cf
pavithra.b <pavithra.b@businessgateways.com>
2025-11-10 12:52:22 +0530

    commited

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>Welcome to CI/CD Demo using Jenkins!</h1>";
+echo "<h1>Welcome to CI/CD Demo using Jenkins!111111111</h1>";
 echo "<p>This is a sample PHP app deployed automatically.</p>";
 ?>
